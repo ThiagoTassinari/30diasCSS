8f5c7ff87ae98d429db7fb11de2497ffb37dcf84
ThiagoTassinari <thiagotassinari12@gmail.com>
2022-02-09 23:42:07 -0300

    Card flip on click

--- Dia 04/Memory Card Game/src/scripts/main.js
+++ Dia 04/Memory Card Game/src/scripts/main.js
@@ -50,6 +50,10 @@ const cardGenerator = () => {
         section.appendChild(card);
         card.appendChild(face);
         card.appendChild(back);
+
+        card.addEventListener("click", (e) => {
+            card.classList.toggle("toggleCard");
+        })
     });
 }
 
