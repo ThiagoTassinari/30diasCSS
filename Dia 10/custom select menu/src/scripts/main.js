3d59e6bf8bd06f9c50dfd03082bae9399273b880
ThiagoTassinari <thiagotassinari12@gmail.com>
2022-02-23 01:24:04 -0300

    Show the searched country only

--- Dia 10/custom select menu/src/scripts/main.js
+++ Dia 10/custom select menu/src/scripts/main.js
@@ -1,66 +1,18 @@
 const wrapper = document.querySelector(".wrapper");
 selectBtn = wrapper.querySelector(".select-btn");
+searchInput = wrapper.querySelector("input");
 options = wrapper.querySelector(".options");
 
 let countries = [
-    {name: 'Afghanistan'}, 
-    {name: 'Åland Islands'}, 
-    {name: 'Albania'}, 
-    {name: 'Algeria'}, 
-    {name: 'American Samoa'}, 
-    {name: 'AndorrA'}, 
-    {name: 'Angola'}, 
-    {name: 'Anguilla'}, 
-    {name: 'Antarctica'}, 
-    {name: 'Antigua and Barbuda'}, 
-    {name: 'Argentina'}, 
-    {name: 'Armenia'}, 
-    {name: 'Aruba'}, 
-    {name: 'Australia'}, 
-    {name: 'Austria'}, 
-    {name: 'Azerbaijan'}, 
-    {name: 'Bahamas'}, 
-    {name: 'Bahrain'}, 
-    {name: 'Bangladesh'}, 
-    {name: 'Barbados'}, 
-    {name: 'Belarus'}, 
-    {name: 'Belgium'}, 
-    {name: 'Belize'}, 
-    {name: 'Benin'}, 
-    {name: 'Bermuda'}, 
-    {name: 'Bhutan'}, 
-    {name: 'Bolivia'}, 
-    {name: 'Bosnia and Herzegovina'}, 
-    {name: 'Botswana'}, 
-    {name: 'Bouvet Island'}, 
-    {name: 'Brazil'}, 
-    {name: 'British Indian Ocean Territory'}, 
-    {name: 'Brunei Darussalam'}, 
-    {name: 'Bulgaria'}, 
-    {name: 'Burkina Faso'}, 
-    {name: 'Burundi'}, 
-    {name: 'Cambodia'}, 
-    {name: 'Cameroon'}, 
-    {name: 'Canada'}, 
-    {name: 'Cape Verde'}, 
-    {name: 'Cayman Islands'}, 
-    {name: 'Central African Republic'}, 
-    {name: 'Chad'}, 
-    {name: 'Chile'}, 
-    {name: 'China'}, 
-    {name: 'Christmas Island'}, 
-    {name: 'Cocos (Keeling) Islands'}, 
-    {name: 'Colombia'}, 
-    {name: 'Comoros'}, 
-    {name: 'Congo'}, 
-    {name: 'Congo, The Democratic Republic of the'}, 
-    {name: 'Cook Islands'}, 
-]
+    'Afghanistan', 'Åland Islands', 'Albania', 'Algeria', 'American Samoa', 'AndorrA',
+    'Angola', 'Australia', 'Bahamas', 'Bangladesh', 'Belgium', 'Bolivia', 'Brazil',
+    'Chile', 'Cameroon', 'Comoros', 'Dinarmark'
+] 
 
 function addCountry() {
     countries.forEach(country => {
         // Adding each country inside and inserting all li inside options tag
-        let li = `<li onclick="updateName(this)">${country.name}</li>`;
+        let li = `<li onclick="updateName(this)">${country}</li>`;
         options.insertAdjacentHTML("beforeend", li); 
     })
 }
@@ -71,6 +23,16 @@ function updateName(selectedLi) {
     selectBtn.firstElementChild.innerText = selectedLi.innerText;
 }
 
+searchInput.addEventListener("keyup", () => {
+    let arr = [];
+    let searchedVal = searchInput.value.toLowerCase();
+    // returning all countries from array which are start with user searched value
+    // and mapping returned country with li and joining them
+    arr = countries.filter(data => {
+        return data.toLowerCase().startsWith(searchedVal);
+    }).map(data => `<li>${data}</li>`).join("");
+    console.log(arr);
+})
 
 selectBtn.addEventListener("click", () => {
     wrapper.classList.toggle("active");
