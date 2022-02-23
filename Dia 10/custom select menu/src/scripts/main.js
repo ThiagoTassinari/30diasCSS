9df5de6f7a382b34cb4640e9189970b9873b0994
ThiagoTassinari <thiagotassinari12@gmail.com>
2022-02-23 00:47:48 -0300

    I went through the list of countries and added it to the DOM

--- Dia 10/custom select menu/src/scripts/main.js
+++ Dia 10/custom select menu/src/scripts/main.js
@@ -1,5 +1,71 @@
 const wrapper = document.querySelector(".wrapper");
 selectBtn = wrapper.querySelector(".select-btn");
+options = wrapper.querySelector(".options");
+
+let countries = [
+    {name: 'Afghanistan'}, 
+    {name: 'Åland Islands'}, 
+    {name: 'Albania'}, 
+    {name: 'Algeria'}, 
+    {name: 'American Samoa'}, 
+    {name: 'AndorrA'}, 
+    {name: 'Angola'}, 
+    {name: 'Anguilla'}, 
+    {name: 'Antarctica'}, 
+    {name: 'Antigua and Barbuda'}, 
+    {name: 'Argentina'}, 
+    {name: 'Armenia'}, 
+    {name: 'Aruba'}, 
+    {name: 'Australia'}, 
+    {name: 'Austria'}, 
+    {name: 'Azerbaijan'}, 
+    {name: 'Bahamas'}, 
+    {name: 'Bahrain'}, 
+    {name: 'Bangladesh'}, 
+    {name: 'Barbados'}, 
+    {name: 'Belarus'}, 
+    {name: 'Belgium'}, 
+    {name: 'Belize'}, 
+    {name: 'Benin'}, 
+    {name: 'Bermuda'}, 
+    {name: 'Bhutan'}, 
+    {name: 'Bolivia'}, 
+    {name: 'Bosnia and Herzegovina'}, 
+    {name: 'Botswana'}, 
+    {name: 'Bouvet Island'}, 
+    {name: 'Brazil'}, 
+    {name: 'British Indian Ocean Territory'}, 
+    {name: 'Brunei Darussalam'}, 
+    {name: 'Bulgaria'}, 
+    {name: 'Burkina Faso'}, 
+    {name: 'Burundi'}, 
+    {name: 'Cambodia'}, 
+    {name: 'Cameroon'}, 
+    {name: 'Canada'}, 
+    {name: 'Cape Verde'}, 
+    {name: 'Cayman Islands'}, 
+    {name: 'Central African Republic'}, 
+    {name: 'Chad'}, 
+    {name: 'Chile'}, 
+    {name: 'China'}, 
+    {name: 'Christmas Island'}, 
+    {name: 'Cocos (Keeling) Islands'}, 
+    {name: 'Colombia'}, 
+    {name: 'Comoros'}, 
+    {name: 'Congo'}, 
+    {name: 'Congo, The Democratic Republic of the'}, 
+    {name: 'Cook Islands'}, 
+]
+
+function addCountry() {
+    countries.forEach(country => {
+        // Adding each country inside and inserting all li inside options tag
+        let li = `<li>${country.name}</li>`;
+        options.insertAdjacentHTML("beforeend", li); 
+    })
+}
+
+addCountry();
 
 selectBtn.addEventListener("click", () => {
     wrapper.classList.toggle("active");
